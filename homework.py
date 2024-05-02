ac4c0e738f7d92da96e2590b87f875a2f6b774ba
Александр Денисов <akatsuki_sanya@vk.com>
2024-05-02 14:56:50 +0500

    Add tokens

--- homework.py
+++ homework.py
@@ -1,11 +1,14 @@
-...
+import os
+
+from dotenv import load_dotenv
+
 
 load_dotenv()
 
 
-PRACTICUM_TOKEN = ...
-TELEGRAM_TOKEN = ...
-TELEGRAM_CHAT_ID = ...
+PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
+TELEGRAM_TOKEN = os.getenv('TOKEN')
+TELEGRAM_CHAT_ID = os.getenv('CHAT_ID')
 
 RETRY_PERIOD = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
