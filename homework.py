f8d3580043e019778f061bee534c61c4412de126
Александр Денисов <akatsuki_sanya@vk.com>
2024-05-03 10:50:48 +0500

    Crate check tokens function

--- homework.py
+++ homework.py
@@ -2,9 +2,7 @@ import os
 
 from dotenv import load_dotenv
 
-
 load_dotenv()
-
 
 PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TOKEN')
@@ -14,7 +12,6 @@ RETRY_PERIOD = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
 
-
 HOMEWORK_VERDICTS = {
     'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
     'reviewing': 'Работа взята на проверку ревьюером.',
@@ -23,7 +20,12 @@ HOMEWORK_VERDICTS = {
 
 
 def check_tokens():
-    ...
+    """Проверка доступности переменных окружения"""
+
+    tokens = (PRACTICUM_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID)
+    if None in tokens:
+        return False
+    return True
 
 
 def send_message(bot, message):
@@ -44,27 +46,28 @@ def parse_status(homework):
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
-def main():
-    """Основная логика работы бота."""
-
-    ...
-
-    # Создаем объект класса бота
-    bot = ...
-    timestamp = int(time.time())
-
-    ...
-
-    while True:
-        try:
-
-            ...
-
-        except Exception as error:
-            message = f'Сбой в работе программы: {error}'
-            ...
-        ...
+# def main():
+#     """Основная логика работы бота."""
+#
+#     ...
+#
+#     # Создаем объект класса бота
+#     bot = ...
+#     timestamp = int(time.time())
+#
+#     ...
+#
+#     while True:
+#         try:
+#
+#             ...
+#
+#         except Exception as error:
+#             message = f'Сбой в работе программы: {error}'
+#             ...
+#         ...
 
 
 if __name__ == '__main__':
-    main()
+    # main()
+    print(check_tokens())
